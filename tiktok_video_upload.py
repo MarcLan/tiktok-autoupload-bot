a5c055c6164d9d588d88d48c693e9fd68efd374f
MarcLan <112602499+MarcLan@users.noreply.github.com>
2023-08-09 11:41:12 +0800

    first commit

---
+++ tiktok_video_upload.py
@@ -0,0 +1,6 @@
+from tiktok_uploader.upload import upload_video
+
+# Upload single video
+upload_video('C:\\code\\content\\videos\\2023-08-08.mp4',
+            description = 'first upload',
+            cookies = 'cookies.txt')
